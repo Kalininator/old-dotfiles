d1780d98d000e0d2e1f4f160bf7bdf3411d12ab8
Alexander Kalinin <alex@kalinin.uk>
2022-03-02 14:21:46 +0000

    Update plugins.lua

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -13,12 +13,8 @@ return require("packer").startup(
     use {"nvim-lua/lsp_extensions.nvim"}
     use {"nvim-lua/completion-nvim"}
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-    use {
-      "terrortylor/nvim-comment",
-      config = function()
-        require("nvim_comment").setup()
-      end
-    }
+    use { "terrortylor/nvim-comment" }
+    require('nvim_comment').setup()
 
     -- telescope
     use "nvim-telescope/telescope.nvim"
