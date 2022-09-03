f5873b8791cff6f78a7fa59831118a5bd6ed835c
Alexander Kalinin <alex@kalinin.uk>
2022-09-03 10:46:05 +0100

    use files instead of git files

--- vim/.config/nvim/lua/binds.lua
+++ vim/.config/nvim/lua/binds.lua
@@ -1,8 +1,8 @@
 local binds = {
   {"i", "kj", "<Esc>"},
   -- FILES = `f`
-  -- {'n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>'}, -- open file finder
-  {"n", "<leader>f", '<cmd>lua require("telescope.builtin").git_files()<CR>'}, -- open git file finder
+  {'n', '<leader>f', '<cmd>lua require("telescope.builtin").find_files()<CR>'}, -- open file finder
+  -- {"n", "<leader>f", '<cmd>lua require("telescope.builtin").git_files()<CR>'}, -- open git file finder
   -- {'n', '<leader>fb', '<cmd>lua require("telescope.builtin").file_browser()<CR>'}, -- open file browser on current working directory
   -- {'n', '<leader>fd', '<cmd>lua require("telescope.builtin").file_browser({ cwd = require("telescope.utils").buffer_dir() })<CR>'}, -- open file browser on buffer directory
   -- {'n', '<Leader>a', '<cmd>lua require("plugins.telescope-config").live_grep()<CR>'}, -- open fuzzy find accross current working directory
