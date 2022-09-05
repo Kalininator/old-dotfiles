a217e58308d44d4d9d3a14670656bbc393f5e90b
Alexander Kalinin <alex@kalinin.uk>
2022-09-05 21:11:16 +0100

    treesitter langs

--- vim/.config/nvim/lua/plugins/treesitter-config.lua
+++ vim/.config/nvim/lua/plugins/treesitter-config.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup(
   {
-    ensure_installed = { "javascript", "lua", "rust", "ruby" },
+    ensure_installed = { "javascript", "typescript", "lua", "rust", "ruby", "yaml", "tsx", "toml", "python", "markdown", "json", "html", "hcl", "gitignore", "dockerfile", "css" },
     highlight = {
       enable = true
     }
