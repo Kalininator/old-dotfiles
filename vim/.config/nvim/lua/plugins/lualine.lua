1fe71de923355317cb57b8841f60790bfb24b9e0
Alexander Kalinin <alex@kalinin.uk>
2023-03-30 21:34:04 +0200

    show macro recording in lualine

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,12 @@
+local function show_macro_recording()
+    local recording_register = vim.fn.reg_recording()
+    if recording_register == "" then
+        return ""
+    else
+        return "Recording @" .. recording_register
+    end
+end
+
 require('lualine').setup {
   options = {
     theme = 'gruvbox_dark',
@@ -5,8 +14,14 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = { 'diff', 'diagnostics'},
-    lualine_c = {{'filename', path = 1}},
+    lualine_b = {
+	{
+	    "macro-recording",
+	    fmt = show_macro_recording,
+	},
+    },
+    lualine_c = { 'diff', 'diagnostics'},
+    lualine_d = {{'filename', path = 1}},
     lualine_x = {'filetype'},
     -- lualine_y = {'progress'},
     lualine_z = {'location'}
