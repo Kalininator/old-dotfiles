e3e68f6ee8ee0ef2972a3b2dd79e75d77709d1fe
Alexander Kalinin <alex@kalinin.uk>
2023-06-30 12:24:29 +0700

    nvim-cmp-config for copilot

--- vim/.config/nvim/lua/plugins/nvim-cmp-config.lua
+++ vim/.config/nvim/lua/plugins/nvim-cmp-config.lua
@@ -1,5 +1,11 @@
 local cmp = require("cmp")
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup(
   {
     snippet = {
@@ -23,19 +29,27 @@ cmp.setup(
           select = true
         }
       ),
-      ["<Tab>"] = cmp.mapping.confirm(
-        {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true
-        }
-      )
+      ["<Tab>"] = vim.schedule_wrap(function(fallback)
+	if cmp.visible() and has_words_before() then
+	  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+	else
+	  fallback()
+	end
+      end),
+      -- ["<Tab>"] = cmp.mapping.confirm(
+      --   {
+      --     behavior = cmp.ConfirmBehavior.Replace,
+      --     select = true
+      --   }
+      -- )
     },
     sources = {
       {name = "nvim_lsp"},
       {name = "buffer"},
       {name = "path"},
       {name = "spell"},
-      {name = "vsnip"}
+      {name = "vsnip"},
+      { name = "copilot" }
     }
   }
 )
